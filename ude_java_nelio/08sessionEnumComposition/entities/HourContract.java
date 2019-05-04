d2309b92b3806aaa9c664b5732d3c6f537c34be2
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-05-04 11:29:25 -0300

    Working with enum and classes's composition

---
+++ ude_java_nelio/08sessionEnumComposition/entities/HourContract.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+
+	// Properties
+	private Date date;
+	private Double valuePerHour;
+	private Integer hours;
+	
+	// Constructor
+	public HourContract() {		
+	}
+
+	public HourContract(Date date, Double valuePerHour, Integer hours) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+
+	// Getters & Setters
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+	
+	// Methods
+	public double totalValue() {
+		return this.hours * this.valuePerHour;
+	}
+}
