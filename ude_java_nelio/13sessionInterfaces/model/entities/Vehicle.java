9c123863a84d2156785ad2f0537021b028efc080
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-06-11 22:43:59 -0300

    Exercise without Interfaces

---
+++ ude_java_nelio/13sessionInterfaces/model/entities/Vehicle.java
@@ -0,0 +1,25 @@
+package model.entities;
+
+public class Vehicle {
+
+	private String model;
+	
+	// Constructors
+	public Vehicle() {		
+	}
+
+	public Vehicle(String model) {		
+		this.model = model;
+	}
+
+	// Getters & Setters
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+}
