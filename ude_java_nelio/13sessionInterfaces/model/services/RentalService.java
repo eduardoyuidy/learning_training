0dbc5e8a6972db580726b815a8310ec9c6682c5c
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-06-15 15:30:16 -0300

    Exercise using Interfaces (Completed)

--- ude_java_nelio/13sessionInterfaces/model/services/RentalService.java
+++ ude_java_nelio/13sessionInterfaces/model/services/RentalService.java
@@ -8,9 +8,9 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {		
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {		
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
@@ -19,10 +19,7 @@ public class RentalService {
 	public void processInvoice(CarRental carRental) {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
-		
-		// System.out.printf("t1 value %.2f", t1);
-		// System.out.printf("t2 value %.2f", t2);
-		
+						
 		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
 		
 		double basePayment;
