35faf60b16a2ede9d04755f5ad4856183b76e3d2
Eduardo Yuidy <eduardo.satake@neofinancial.com>
2020-04-04 14:59:24 -0600

    Initial commit

---
+++ ude_graphql/users/lib/mock-data.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    id: '23',
+    firstName: 'Eduardo',
+    age: 32
+  },
+  {
+    id: '82',
+    firstName: 'Poliane',
+    age: 29
+  },
+  {
+    id: '92',
+    firstName: 'Elaine',
+    age: 26
+  }
+];
+
+module.exports = { users };
