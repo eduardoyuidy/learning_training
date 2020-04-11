fb24aeecef38d22233c3df5a20783cf81ca9c285
Eduardo Yuidy <eduardo.satake@neofinancial.com>
2020-04-11 15:10:17 -0600

    Added Graphql Query to components and added React-router

---
+++ ude_graphql/LyricalGraphQL/client/components/SongList.js
@@ -0,0 +1,41 @@
+import React , { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  };
+
+  render() {
+
+    if (this.props.data.loading) { 
+      return (<div>Loading...</div>);
+    }
+
+    return (
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
+    );
+  }
+}
+ 
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+// Associate the Query with our component using 'react-apollo'
+export default graphql(query)(SongList);
