eea8d68b90d04b300b65078c1e2592a6da2b6fe1
Eduardo Yuidy <eduardo.satake@neofinancial.com>
2020-04-22 21:14:30 -0600

    Login Logout mutations

--- ude_graphql/auth-graphql-starter-master/server/schema/mutations.js
+++ ude_graphql/auth-graphql-starter-master/server/schema/mutations.js
@@ -17,7 +17,25 @@ const mutation = new GraphQLObjectType({
 
         return AuthService.signup({ email, password, req: request });
       }
-    }    
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, request) {
+        const { user } = request;
+        request.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, request) {
+        return AuthService.login({ email, password, req: request });
+      }
+    }
   }
 });
 
